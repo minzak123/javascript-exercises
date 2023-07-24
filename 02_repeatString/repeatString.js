c6398bb6c7c4e52e21d1a4744c94238b47b5a047
Michael <minzak123@hotmail.ca>
2023-07-24 17:29:26 -0400

    feat: completed all tests for repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(inputString, inputTimes) {
+
+    if (inputTimes < 0) {
+        return "ERROR";
+    }
+    if (inputString == "") {
+        return inputString
+    }
+    return inputString.repeat(inputTimes);
 
 };
 
