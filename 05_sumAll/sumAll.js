5c42d015b6497988407410a3976c5fd428addebb
Michael <minzak123@hotmail.ca>
2023-07-28 15:48:01 -0400

    feat: completed sumAll
    Looped from lowest-highest number, summing the counter + sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    var sum = 0
+
+    //returns ERROR if negative/non number paramaters
+
+    if(start < 0 || end < 0){
+        return "ERROR";
+    }
+
+    if(typeof(start) !== "number" || typeof(end) !== "number"){
+        return "ERROR";
+    }
+
+
+    if(start == end){
+        return start + end
+    }
+    else if (start > end){
+        // swap start/end
+        for(counter = end; counter <= start; counter++){
+            sum += counter;
+
+        }
+    }
+    else{
+        //expected defaut order. Sum from Start-end
+
+        for(counter = start;counter <= end; counter++){
+            sum += counter;
+        }
+    }
+
+
+    return sum;
 
 };
 
