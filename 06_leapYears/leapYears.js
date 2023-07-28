41ae3158dda102065a9551b265e5f2f227c527d6
Michael <minzak123@hotmail.ca>
2023-07-28 16:41:03 -0400

    feat: completed leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    //check if div by 4
+    if(year % 4 == 0){
+
+        //check if div by 400
+        if(year % 400 == 0){
+            return true;
+        }
+        else if(year % 100 == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+    else {
+        return false;
+    }
 
 };
 
