e24fadadcd02e542a337fa9f38f44d93f7152960
Michael <minzak123@hotmail.ca>
2023-07-28 14:37:16 -0400

    feat: completed removeFromArray
    ...args rest operator was the key to the solution
    spent a few hours not realizing the test values were
    multiple values, not just 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,35 @@
-const removeFromArray = function() {
 
-};
+
+const removeFromArray = function(array, ...args) {
+
+    values = [...args];
+    console.log(values)
+    //check each element of values against array elements
+
+    for(i = 0; i < values.length; i++){
+        const value = values[i];
+
+        for(j = 0; j < array.length; j++){
+            const element = array[j];
+
+
+            //remove element from array if it matches value
+
+            if(value == element && typeof(value) == typeof(element)){
+
+                array.splice(j,1);
+
+            }
+
+
+        }
+
+
+    }
+    return array;
+
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
